b44cd5636afd954d42dd91efb9fa45d1aea2bc08
Sasha Klein <sashafklein@gmail.com>
2014-10-31 09:15:24 -0700

    Deleted empty file, pointless test. Renamed some lib specs

---
+++ spec/lib/services/clusterer_spec.rb
@@ -0,0 +1,55 @@
+module Services
+  describe Clusterer do
+    describe "clusters" do
+      describe "with one clear cluster" do
+        
+        before { @clusterer = cluster_from_names [] }
+
+        it "groups appropriately" do
+          expect(@clusterer.to_json).to eq([
+            [{
+               location_name: [lat, lon],
+               location_name: [lat, lon]
+            }],
+          ])
+        end
+      end
+
+      describe "with two clear clusters" do
+        
+        before { @clusterer = cluster_from_names [] }
+
+        it "groups appropriately" do
+        end
+      end
+
+      describe "with three clear clusters" do
+        
+        before { @clusterer = cluster_from_names [] }
+
+        it "groups appropriately" do
+        end
+      end
+
+      describe "with 10 clear clusters" do
+        
+        before { @clusterer = cluster_from_names [] }
+
+        it "groups appropriately" do
+        end
+      end
+
+      describe "with borderline 1-2 clusters" do
+        
+        before { @clusterer = cluster_from_names [] }
+
+        it "groups appropriately" do
+        end
+      end
+    end
+  end
+
+  def clusterer_from_names(location_names)
+    locations = location_names.map{ |n| create("#{n}_location") }
+  end
+end
