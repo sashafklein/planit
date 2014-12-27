2ab237a56a779ca1ece1a2178a1460a46632b41d
Sasha Klein <sashafklein@gmail.com>
2014-12-26 16:27:45 -0800

    Fixed spec asserting that scraper could use just URL -- switched to Contigo, because Trip Advisor too unpredictable

--- spec/lib/services/site_scraper_spec.rb
+++ spec/lib/services/site_scraper_spec.rb
@@ -4,21 +4,19 @@ module Services
   describe SiteScraper do
     describe "initialize" do
 
-      let(:url) { 'http://www.tripadvisor.com/Restaurant_Review-g1066456-d1679642-Reviews-Fuunji-Shibuya_Tokyo_Tokyo_Prefecture_Kanto.html' }
-      let(:page) { File.read File.join(File.join('spec', 'support', 'pages', 'tripadvisor', "fuunji.html")) } 
+      let(:url) { 'http://www.contigo.com' }
+      let(:page) { File.read File.join(File.join('spec', 'support', 'pages', 'contigosf', "contigo.html")) } 
 
       it "handles no page", :vcr do
         page_scraper = SiteScraper.build(url, page)
-        expect(page_scraper).to be_a Scrapers::TripadvisorMod::ItemReview
+        expect(page_scraper).to be_a Scrapers::General
         
         url_scraper = SiteScraper.build(url)
-        expect(url_scraper).to be_a Scrapers::TripadvisorMod::ItemReview
+        expect(url_scraper).to be_a Scrapers::General
 
         expect( 
-          url_scraper.page.css('body .content_block').inner_html.first(200)
-        ).to eq( 
-          page_scraper.page.css('body .content_block').inner_html.first(200)
-        )
+          url_scraper.page.css('body .content_block').inner_html.first(300)
+        ).to eq page_scraper.page.css('body .content_block').inner_html.first(300)
       end
 
       it "errors without url" do
