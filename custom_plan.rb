6164b9eb3888850738582d005ff85d9e3f92eca1
Sasha Klein <sashafklein@gmail.com>
2015-01-29 14:43:57 -0800

    Removed some bower stuff. Clean up

--- custom_plan.rb
+++ custom_plan.rb
@@ -2,10 +2,10 @@ require 'zeus/rails'
 
 class CustomPlan < Zeus::Rails
 
-  # def my_custom_command
-  #  # see https://github.com/burke/zeus/blob/master/docs/ruby/modifying.md
-  # end
-
+  def rspec(argv=ARGV)
+    test(argv)
+  end
+  
 end
 
 Zeus.plan = CustomPlan.new
