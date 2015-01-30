b657c62377b29ff6e36f3190dcc03d228f521cc0
Sasha Klein <sashafklein@gmail.com>
2015-01-30 11:17:00 -0800

    Toned down sleeps

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -5,15 +5,13 @@ describe 'Authentication', driver: :selenium do
   describe "sign in" do
     it "is linked to on the landing page" do
       user = create(:user)
-      sleep 1
       visit root_path
-      sleep 2
+
+      sleep 1
+      
       expect( page ).to have_content 'LOG IN' # On sign-in page
-      sleep 1
       fill_in 'Email', with: user.email
-      sleep 1
       fill_in "Password", with: user.password
-      sleep 1
       click_button 'Sign in'
       
       expect( current_path ).to eq user_path(user)
@@ -23,15 +21,15 @@ describe 'Authentication', driver: :selenium do
   describe 'registration' do
     it "allows new users to register" do
       visit root_path
+
+      sleep 1
+      
       click_link 'Sign up'
-      sleep 2
       fill_in 'Email', with: 'myfake@email.com'
       fill_in 'Password', with: 'password'
-      sleep 1
       fill_in 'Password confirmation', with: 'password'
       fill_in 'First name', with: 'John'
       fill_in 'Last name', with: 'Doe'
-
       click_button "Sign up"
 
       expect( current_path ).to include '/users/'
