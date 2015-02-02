4fe0dbfcf6c3dc87c8d8e99507bf035d1b447e5a
Sasha Klein <sashafklein@gmail.com>
2015-02-02 13:31:10 -0800

    Fixed meta_ext

---
+++ lib/extensions/core/object.rb
@@ -0,0 +1,13 @@
+class Object
+  
+  def to_super
+    if self.is_a?(Hash)
+      self.to_sh
+    elsif self.is_a?(Array)
+      self.to_sa
+    else
+      self
+    end
+  end
+
+end
--- lib/meta_ext/json_accessor.rb
+++ lib/meta_ext/json_accessor.rb
@@ -7,7 +7,7 @@ module MetaExt
         symbols.each do |field|
           class_eval do 
             define_method(field) do
-              self[field].is_a?(Hash) ? self[field].to_sh : self[field].to_sa
+              self[field].to_super
             end
 
             define_method("add_to_#{field}!") do |arg|
@@ -28,7 +28,7 @@ module MetaExt
               other.delete(arg.to_s); other.delete(arg.to_sym)
               self[field.to_sym] = other
               send("#{field}_will_change!")
-              self[field.to_sym]
+              send field
             end
 
             define_method("remove_from_#{field}!") do |arg|
@@ -37,19 +37,19 @@ module MetaExt
               self[field.to_sym] = other
               send("#{field}_will_change!")
               save
-              self[field.to_sym]
+              send field
             end
 
             define_method("set_#{field}") do |arg|
               self[field.to_sym] = arg
               send("#{field}_will_change!")
-              self[field.to_sym]
+              send field
             end
 
             define_method("set_#{field}!") do |arg|
               self[field.to_sym] = arg
               send("#{field}_will_change!")
-              self[field.to_sym]
+              send field
             end
           end
         end
