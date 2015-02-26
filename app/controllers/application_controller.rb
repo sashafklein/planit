85526bce2a2ec50a23f19bb4ba632f1b332986d8
Niko Klein <nikoklein2@gmail.com>
2015-02-26 15:57:44 -0800

    removed raise from post-sign-up redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :update_sanitized_params, if: :devise_controller?
 
-  def after_sign_up_path_for(resource)
-    raise
-    waitlist_path
-  end
-
   def after_sign_in_path_for(resource)
     root_path
   end
--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,10 +3,8 @@ class LandingController < ApplicationController
   def show
     if !current_user
       redirect_to index_path
-      # redirect_to new_user_session_path
     elsif current_user.pending?
       redirect_to waitlist_path
-      # raise
     else
       redirect_to user_path(current_user)
     end
