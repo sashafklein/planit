03b8d470ed1e7338e8599be088eef6f2d9c0c92a
Niko Klein <nikoklein2@gmail.com>
2015-03-10 10:41:46 -0700

    invitations with spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,26 @@
 require 'rails_helper'
 
 describe User do
-  describe '#save_as_beta' do
-    it "saves the user as pending and shoots off two emails" do
+  describe '#save_as' do
+    it "beta saves the user as pending and shoots off two emails" do
       user = build(:user, password: nil, role: :pending)
 
       expect(AdminMailer).to receive(:notify_of_signup).at_least(:once).and_call_original
-      expect(UserMailer).to receive(:welcome_beta).at_least(:once).and_call_original
+      expect(UserMailer).to receive(:welcome_waitlist).at_least(:once).and_call_original
 
       expect{
-        user.save_as_beta
+        user.save_as(:pending)
+      }.to change{ User.count }.by 1
+    end
+
+    it "invite saves the user as pending and shoots off two emails" do
+      user = build(:user, password: nil, role: :pending)
+
+      expect(AdminMailer).to receive(:notify_of_signup).at_least(:once).and_call_original
+      expect(UserMailer).to receive(:welcome_invited).at_least(:once).and_call_original
+
+      expect{
+        user.save_as(:member)
       }.to change{ User.count }.by 1
     end
   end
