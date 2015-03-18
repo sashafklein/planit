176c2bca45c0a02cca2b1f2d35aee30f12508efb
Sasha Klein <sashafklein@gmail.com>
2015-03-17 17:07:58 -0700

    Removed NoViewMailer to get specs passing - it's better to preview

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,26 +2,26 @@ class AdminMailer < ActionMailer::Base
   default from: "Notifier <notifier@plan.it>"
   default to: "Planit <hello@plan.it>"
 
-  include NoViewMailer
+  layout 'layouts/mailer'
 
   def bookmarklet_failure(user_id, url)
-    mail_no_layout( subject: 'Bookmarklet Failure', content: ["User ID: #{user_id}", "URL: #{url}"]  )
+    @user_id, @url = user_id, url
+    mail( subject: 'Bookmarklet Failure' )
   end
 
   def new_feedback(page_id)
-    page = PageFeedback.find(page_id)
-    mail_no_layout(subject: "New Feedback", content: ["User ID: #{page.user_id}", "Details: #{page.details}", "NPS ID: #{page.nps_feedback_id}"] )
+    @page = PageFeedback.find(page_id)
+    mail( subject: "New Feedback" )
   end
 
   def failed_feedback(nps_id, current_user_id, url)
-    mail_no_layout(subject: "Feedback failed to save!", content: ["User ID: #{current_user_id}", "NPS ID: #{nps_id}", "URL: #{url}"])
+    @nps_id, @current_user_id, @url = nps_id, current_user_id, url
+    mail( subject: "Feedback failed to save!" )
   end
 
   def notify_of_signup(user)
-    list = [:id, :first_name, :last_name, :email, :role].map do |attr|
-      "<li>#{attr.to_s.capitalize}: #{user[attr]}</li>"
-    end
-    mail_no_layout({subject: "New Planit #{user.role.capitalize} Sign up!", content: "<ul>#{list}</ul>"})
+    @user = user
+    mail( subject: "New Planit #{user.role.capitalize} Sign up!" )
   end
 
   def report_error(details)
---
+++ spec/previews/admin_mailer_preview.rb
@@ -0,0 +1,19 @@
+class UserMailerPreview < ActionMailer::Preview
+
+  def bookmarklet_failure
+    AdminMailer.bookmarklet_failure(User.first.id, 'nytimes.com')
+  end
+
+  def new_feedback
+    AdminMailer.new_feedback(PageFeedback.first)
+  end
+
+  def failed_feedback
+    AdminMailer.failed_feedback(NpsFeedback.first.id, User.first.id, 'localhost:3000/whatever')
+  end
+
+  def notify_of_signup
+    AdminMailer.notify_of_signup(User.first)
+  end
+
+end
