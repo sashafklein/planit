6cf75e75ed1c68e6e16002494ce9cf8a2faa05f4
Niko Klein <nikoklein2@gmail.com>
2015-04-18 13:26:54 -0700

    fixed the mailer invitation, and host path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,5 +85,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'http://vast-peak-6618.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://plan.it' }
 end
