c174cea9c0b36f113320579d07ae454c75391285
Sasha Klein <sashafklein@gmail.com>
2015-04-22 11:47:26 -0700

    Master spec exception; added MailListEmail and AcceptedEmail to Admin

--- spec/requests/master_spec.rb
+++ spec/requests/master_spec.rb
@@ -67,7 +67,7 @@ describe "All views" do
 
     context ||= "as #{role}"
     
-    RouterHelper.get_routes_by_controller( %w(mini_scrape oauths allowable bookmarklets search unmatched_route morph) ).each do |ctrl_group|
+    RouterHelper.get_routes_by_controller( %w(mini_scrape oauths allowable bookmarklets search unmatched_route morph /admin/) ).each do |ctrl_group|
       ctrl_group.last.each do |rh|
         action = [rh.ctrl, rh.sub_path].join('#')
         vars, params = MasterHelper.set_vars_and_params(rh.required_objects.dup)
