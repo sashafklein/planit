d26ece1eead14b49182211726d1f14395bd1d099
Sasha Klein <sashafklein@gmail.com>
2015-05-22 19:34:43 -0700

    Fixed plan spec

--- app/validators/mark_validator.rb
+++ app/validators/mark_validator.rb
@@ -10,7 +10,7 @@ class MarkValidator < BaseValidator
     return unless mark.place_id && mark.user_id
     other = Mark.find_by(place_id: mark.place_id, user_id: mark.user_id)
     if other && other.id != mark.id
-      mark.errors[:base] << "A Mark with that User ID and Place ID already exists. mark_id: #{other.id}, place_id: #{other.place_id}, user_id: #{other.user_id}"
+      mark.errors[:base] << "A Mark with that user_id and place_id already exists. mark_id: #{other.id}, user_id: #{other.user_id}, place_id: #{other.place_id}"
     end
   end
 
