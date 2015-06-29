2805363b76b68c3fc484628622a6ed3f3ab91449
Sasha Klein <sashafklein@gmail.com>
2015-06-29 15:26:41 -0700

    Fixed scraper for older NYTimes 36 hours. Handles legends correctly

--- lib/scrapers/nytimes_mod/separated_thirty_six_details.rb
+++ lib/scrapers/nytimes_mod/separated_thirty_six_details.rb
@@ -24,27 +24,26 @@ module Scrapers
           plan:{
             name: remove_plan_name_fluff( page.css("title").text ),
           },
-          scraper_url: @url, 
+          scraper_url: @url
         }
       end
 
       # PAGE 
 
       def activity_group_array
-        return @activity_group_array if @activity_group_array
-        array_in_activity_group_array = []
-        if has_legend?
-          activity_details_container = scrape_content.scan(day_section_start_regex(["the basics", "the details", "if you go", "lodging"])).flatten.first
-          return @activity_array_group = activity_details_container.scan(p_strong_details_regex)
-        end        
+        if legend_header
+          legend_header.parent.children.map(&:inner_html).select{ |e| e.scan(/\d+(?:<strong>)*\./).any? }
+        else
+          []
+        end
       end        
 
       def activity_data(activity)
         {
           place:{
             name: name( activity ),
-            street_address: trim( activity.scan(strong_details_regex_find_address_phone).flatten.first ),
-            website: activity.scan(a_regex_find_href).flatten.first,
+            street_address: street_address( activity ),
+            website: website( activity ),
             notes: clean_note( notes_for( name( activity ) ) ),
             nearby: nearby
           }
@@ -52,21 +51,36 @@ module Scrapers
       end
 
       # NEEDS TO BE MORE STRINGENT
-      def has_legend?
-        downcased = scrape_content.downcase
-        return false unless (["the basics", "the details", "if you go", "lodging"]).any?{ |legend_group| downcased.include?(legend_group) }
-        true
+      def legend_header
+        possible_headers = ["the basics", "the details", "if you go", "lodging"]
+
+        css('p').find do |e| 
+          possible_headers.find{ |h| e.inner_html.downcase.include?(h) }
+        end
       end
 
       def name(activity)
-        trim( de_tag( activity.scan(strong_details_regex_find_name).flatten.first ) )
+        trim( de_tag( activity.scan(strong_details_regex_find_name).flatten.first ) ) || 
+          Nokogiri.parse(activity).css('strong').first.text.split(/\d+\.\s*\W*/).last.try(:strip)
+      end
+
+      def street_address(activity)
+        first_guess = trim( activity.scan(strong_details_regex_find_address_phone).flatten.first )
+        return first_guess if first_guess
+        activity.split(/<\/strong>(?:\W|\D)*/).last.split("<a").first.gsub(/[;|,|.]\s*$/, '')
+      rescue
+        nil
+      end
+
+      def website( activity )
+        activity.scan(a_regex_find_href).flatten.first
       end
 
       def nearby
         split_by('h1', [["36 Hours in ", 1], ["36 Hours at the ", 1], ["36 Hours on ", 1], ["36 Hours | ", 1]])
       end
 
-      memoize :name, :nearby
+      memoize :name, :nearby, :legend_header, :activity_group_array
     end
   end
 end
